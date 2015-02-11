bcbbd400e3ef4e58ed2d5b0df9945c85df9d67f0
Dan McClain <git@danmcclain.net>
2015-02-11 13:34:23 -0500

    Auto deploy config

---
+++ travis_deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+git config --global user.email "socko@dockyard.com"
+git config --global user.name "sockothesock"
+
+
+# This specifies the user who is associated to the GH_TOKEN
+USER="sockothesock"
+
+# sending output to /dev/null to prevent GH_TOKEN leak on error
+git remote rm origin
+git remote add origin https://${USER}:${GH_TOKEN}@github.com/dockyard/reefpoints.git &> /dev/null
+
+rake publish
+
+echo -e "Done\n"
