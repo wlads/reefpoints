129ef1178fefa4536ce387b6f13a7726e66bee13
Dan McClain <git@danmcclain.net>
2015-04-15 13:35:17 -0400

    Adds explicit scp path

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -21,6 +21,6 @@ bundle exec rake build
 
 chmod 600 ./reefpoints_deploy
 ssh-keyscan -H homeport.dockyard.com >> ~/.ssh/known_hosts
-scp -i ./reefpoints_deploy build/posts.json temp_deploy@homeport.dockyard.com:reefpoints/
+scp -i ./reefpoints_deploy build/posts.json temp_deploy@homeport.dockyard.com:reefpoints/posts.json
 
 echo -e "Done\n"
